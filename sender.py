ba2798fca442077f61f2d3e52efe5e639ee93a2a
John Dott <jkd28@pitt.edu>
2018-10-01 16:53:41 -0400

    Add logo print; Add more error checking

--- sender.py
+++ sender.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 import serial
+from serial import SerialException
+from Crypto.Hash import SHA256
+from colorama import init
+from termcolor import cprint
+from pyfiglet import figlet_format
+import sys
+
+
+def print_logo():
+    # strip colors if stdout is redirected
+    init(strip=not sys.stdout.isatty())
+    logo = figlet_format('S.H.I.E.L.D.', font='starwars')
+    cprint(logo, 'yellow', 'on_grey', attrs=['bold'])
+
+def print_critical_failure(error_string):
+    print("CRITICAL FAILURE: " + error_string)
 
 
 def configure_serial(port, baudrate):
@@ -10,15 +26,27 @@ def configure_serial(port, baudrate):
 
 
 def main():
-    # Gather port configurations from command line
-    serial_port = input("Serial Port: ")
-    serial_rate = input("Serial Baudrate: ")
+    print_logo()
+    print("Step 1) Configure serial connection settings: ")
 
-    serial_connection = configure_serial(serial_port, serial_rate)
+    # configure the serial connection settings
+    try:
+        serial_port = input("\tEnter Serial Port: ")
+        serial_rate = input("\tEnter Serial Baudrate: ")
+        connection = configure_serial(serial_port, serial_rate)
+    except ValueError:
+        print_critical_failure("Invalid Port/Baudrate Values")
+        sys.exit()
 
-    serial_connection.open()
-    serial_connection.write(b'Hello')
-    serial_connection.close()
+    # Attempt to connect to the serial device
+    try:
+        connection.open()
+    except SerialException:
+        print_critical_failure("Error connecting to serial device")
+        sys.exit()
+
+    connection.write(b'Hello')
+    connection.close()
 
 
 if __name__ == "__main__":
