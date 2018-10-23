551dc6dc086651deeaaff54509ac49ca0ec97dda
SJustice48 <smj48@pitt.edu>
2018-10-23 17:15:57 -0400

    Add files via upload
    PWM and ADC code framework using 1kHz counter interrupts

---
+++ ISR_TRIG_ADC/ISR_TRIG_ADC.ino
@@ -0,0 +1,51 @@
+#include <avr/interrupt.h>
+#include <Time.h>
+
+void wait(volatile int time_multiple, volatile char time_choice);
+void delay_T_msec_timer1(volatile char time_choice);
+uint8_t Rval;
+_Bool new_bit;
+
+
+//kHz Sample 
+ISR(TIMER0_COMPA_vect)
+{
+    TCNT0 = 0; //MUST RESET TIMER IMMEDIATELY AFTER INTERRUPT FOR ACCURATE TIMING
+    
+    ADCSRA |= (1<<ADSC);
+    while ((ADCSRA & (1<<ADIF)) == 0);
+    Rval = ADCH;
+
+    if(Rval >= 0x80){
+      new_bit = 1;
+    }
+    else{
+      new_bit = 0;
+    }
+}
+
+
+int main(void)
+{
+  DDRC = 0 << PORTC0; //A/D for PinC0
+  DDRD = 0xFF;
+  PORTB=0x00;
+  
+  //#############    ADC Setup    ###########################
+  PRR = 0x00;
+  ADCSRA = 1<<ADEN | 1<<ADPS2 | 1<<ADPS1 | 1<<ADPS0;
+  ADMUX = (ADMUX & 0xF0) | 0<<REFS1 | 1<<REFS0 | 1<<ADLAR; //Set mux bits, for ADC0 single ended
+  //#############################################################
+  
+  //################    TIMER SETUP   ###########################
+  TCCR0A = 0x00;
+  TCNT0 = 0x00; //pre-load timer to 0
+  OCR0A = 0xF9; //for 1ms interrupt
+  TIMSK0 = (1 << OCIE0A);
+  sei();
+  TCCR0B = 1<<CS21 | 1<<CS20; //TCCR0B = 0x03, pre-scaler 64;
+  //#############################################################  
+
+  
+  while (1){}
+}
---
+++ ISR_TRIG_PWM/ISR_TRIG_PWM.ino
@@ -0,0 +1,57 @@
+/*
+   ISR BASED FREQ SHIFT 
+
+   Created: 3/1/2018 12:00:33 PM
+   Author : lepra
+*/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+_Bool F_Sig = 0;
+
+ISR(TIMER0_COMPA_vect)
+{
+  if(F_Sig){
+    OCR2A = 0x30;
+    PORTD = 0xF0;
+  }
+  else{
+    OCR2A = 0x3F;
+    PORTD = 0x00;
+  }
+  
+  F_Sig = !F_Sig;
+  TCNT0 = 0;
+}
+
+int main(void)
+{
+  //OCR2A sets top level of counter/timer 2
+  //Run PWM duty cycle off of OCR2B
+
+  //#############   PWM PRESET    ###########################
+  DDRD = 0xFF; //analog pin out 3 is pwm output for pwm 2B
+  OCR2A = 0x34; //carrier freq 2
+  OCR2A = 0x40; //carrier freq 1
+  OCR2B = 0x00; //set duty to 0%
+
+  //TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2A = _BV(COM2B1) | _BV(WGM21) | _BV(WGM20); //com2b1 enables pwm on analog 3, wgm bits set pwm type
+  TCCR2B = 1 << WGM22 | 0 << CS22 | 1 << CS21 | 0 << CS20; //CS bits for pre-scale of 8
+  //#############################################################
+  
+  //################    TIMER SETUP   ###########################
+  TCCR0A = 0x00;
+  TCNT0 = 0x00; //pre-load timer to 0
+  OCR0A = 0xF9;
+  TIMSK0 = (1 << OCIE0A);
+  sei();
+  TCCR0B = 1<<CS21 | 1<<CS20; //TCCR0B = 0x03, pre-scaler 64;
+  //#############################################################
+
+  OCR2B = 0x1F; //start pwm
+  
+  while (1){}
+  
+}
